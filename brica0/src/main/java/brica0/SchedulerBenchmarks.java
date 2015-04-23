705aa76efd865c594a665ad48b53bf4df155a4e4
Moriyoshi Koizumi <mozo@mozo.jp>
2015-04-23 11:21:49 +0900

    テストの修正

--- brica0/src/main/java/brica0/SchedulerBenchmarks.java
+++ brica0/src/main/java/brica0/SchedulerBenchmarks.java
@@ -1,7 +1,5 @@
 package brica0;
 
-import java.util.Arrays;
-
 public class SchedulerBenchmarks {
     
     public static void main(String args[]) throws Exception {
@@ -61,8 +59,6 @@ public class SchedulerBenchmarks {
         Module m = new NullModule();
 
         ca.addModule("M1", m);
-
-        Module mm = ca.getModule("M1");
 
         long startTime = System.currentTimeMillis();
         
